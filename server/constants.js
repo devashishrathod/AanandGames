b856815a712c48bf705db62da9e68f178021dbb3
Devashish Rathod <rathod.devashish09@gmail.com>
2026-04-06 05:12:27 +0530

    added

--- server/constants.js
+++ server/constants.js
@@ -111,5 +111,7 @@ module.exports = {
       "https://res.cloudinary.com/drvdnqydw/image/upload/f_auto,q_auto/v1/Images/zsbowllown6ddeb4jnw0?_a=BAMAK+Jw0",
     SPORT:
       "https://res.cloudinary.com/drvdnqydw/image/upload/f_auto,q_auto/v1/Images/zsbowllown6ddeb4jnw0?_a=BAMAK+Jw0",
+    VENUE:
+      "https://res.cloudinary.com/drvdnqydw/image/upload/f_auto,q_auto/v1/Images/zsbowllown6ddeb4jnw0?_a=BAMAK+Jw0",
   }),
 };
